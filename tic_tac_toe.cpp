d1be74a77b5731fb06f63974cc9c6cbc88479e51
Augustus Allred <103090532+itsjustmegus@users.noreply.github.com>
2023-09-14 11:09:17 -0600

    main program for tic_tac_toe.cpp from Week4 folder

---
+++ tic_tac_toe.cpp
@@ -0,0 +1,20 @@
+/*
+ * Name:    tic_tac_toe.cpp
+ * Author:  Gus Allred
+ * Cretaed: 09/13/23
+ * Purpose: Simple C++ tic tac toe game
+ */
+
+// Create a title
+
+// Initialize a character list for the squares in the game
+
+// Build the game grid
+
+// Loop until the game ends
+
+    // Get player input
+
+    // Mark player choice on the game grid
+
+// Determine wins and losses
