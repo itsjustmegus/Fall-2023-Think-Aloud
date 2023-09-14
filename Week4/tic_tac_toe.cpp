75e1c04d95c1ebd6cddd88f744c15f89f02845dc
Augustus Allred <103090532+itsjustmegus@users.noreply.github.com>
2023-09-13 22:03:42 -0600

    added pseudocode

--- Week4/tic_tac_toe.cpp
+++ Week4/tic_tac_toe.cpp
@@ -4,3 +4,17 @@
  * Cretaed: 09/13/23
  * Purpose: Simple C++ tic tac toe game
  */
+
+// Create a title
+
+// Initialize a character list for the squares in the game
+
+// Build the game grid
+
+// Loop until the game ends
+
+    // Get player input
+
+    // Mark player choice on the game grid
+
+// Determine wins and losses
