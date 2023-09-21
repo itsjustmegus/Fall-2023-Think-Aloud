d2cd9dfbff3565b23ff1d044e881c29b05dccea7
Augustus Allred <103090532+itsjustmegus@users.noreply.github.com>
2023-09-20 22:07:06 -0500

    updated main game with Week 5 version

--- tic_tac_toe.cpp
+++ tic_tac_toe.cpp
@@ -2,14 +2,51 @@
  * Name:    tic_tac_toe.cpp
  * Author:  Gus Allred
  * Cretaed: 09/13/23
+ * Revised: 09/20/23
  * Purpose: Simple C++ tic tac toe game
+ *          Following the tutorial from https://www.simplilearn.com/tutorials/cpp-tutorial/game-in-cpp
  */
 
-// Create a title
+#include <iostream>
 
 // Initialize a character list for the squares in the game
+char space[3][3] = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+int row;
+int column;
+char token = 'x';
+bool tie = false;
 
-// Build the game grid
+void functionOne();
+
+int main()
+{
+
+    // Create a title
+    std::cout << "+-------------------------------------------+" << std::endl;
+    std::cout << "|           Gus' Tic Tac Toe Game           |" << std::endl;
+    std::cout << "+-------------------------------------------+" << std::endl;
+
+    functionOne();
+
+    return 0;
+
+}
+
+void functionOne()
+{
+
+    // Build the game grid
+    std::cout << "     |     |     \n";
+    std::cout << "  " << space[0][0] << "  |  " << space[0][1] << "  |  " << space[0][2] << "  \n";
+    std::cout << "_____|_____|_____\n";
+    std::cout << "     |     |     \n";
+    std::cout << "  " << space[1][0] << "  |  " << space[1][1] << "  |  " << space[1][2] << "  \n";
+    std::cout << "_____|_____|_____\n";
+    std::cout << "     |     |     \n";
+    std::cout << "  " << space[2][0] << "  |  " << space[2][1] << "  |  " << space[2][2] << "  \n";
+    std::cout << "     |     |     \n";
+
+}
 
 // Loop until the game ends
 
